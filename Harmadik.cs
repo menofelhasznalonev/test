0e70fb1e509774a1797c1228bcb9a9ba13f22694
Zsolt <beki.zsolt@petrik.hu>
2026-01-29 13:47:37 +0100

    Harmadik added

---
+++ Harmadik.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+	internal class Harmadik
+	{
+		Dictionary<Adat, int> stat;
+
+		public Harmadik()
+		{
+			stat = new Dictionary<Adat, int>();
+		}
+	}
+}
