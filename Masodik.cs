0cba480e8a086fb245051b64d84d6451b19855cb
Zsolt <beki.zsolt@petrik.hu>
2026-01-29 13:33:53 +0100

    masodik aded

---
+++ Masodik.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+	internal class Masodik
+	{
+		List<Adat> list;
+
+		public Masodik()
+		{
+			list = new List<Adat>();
+		}
+
+
+	}
+}
