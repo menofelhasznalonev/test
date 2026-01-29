ddfa7fe130e715072dbe9de9dabbe2724291532c
Petrik Diak <diak@petrik.hu>
2026-01-29 13:30:27 +0100

    mukodik

---
+++ adat.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+	internal class Adat
+	{
+		private string nev;
+		private int ar;
+
+		public Adat(string nev, int ar)
+		{
+			this.Nev = nev;
+			this.Ar = ar;
+		}
+
+		public string Nev { get => nev; set => nev = value; }
+		public int Ar { get => ar; set => ar = value; }
+	}
+}
